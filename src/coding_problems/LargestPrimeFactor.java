eb2ae139fd351652e05e5ea5e462f0df06bf9ae2
pawan136475 <pawan36475@gmail.com>
2025-12-13 22:00:24 +0530

    geeks for geeks

---
+++ src/coding_problems/LargestPrimeFactor.java
@@ -0,0 +1,39 @@
+package coding_problems;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+//https://www.geeksforgeeks.org/problems/largest-prime-factor2601/1
+
+public class LargestPrimeFactor {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static int largestPrimeFactor(int n) {
+
+        int largest = -1;
+        while (n % 2 == 0) {
+            largest = 2;
+            n /= 2;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                largest = i;
+                n /= i;
+            }
+        }
+        if (n > 2)
+            largest = n;
+
+        return largest;
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+
+        int n = sc.nextInt();
+
+        int result = largestPrimeFactor(n);
+        System.out.println(result);
+    }
+}
