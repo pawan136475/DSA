97eae9748f6e034f1519a963fd754103d7c13201
pawan136475 <pawan36475@gmail.com>
2025-12-20 14:49:04 +0530

    code

--- src/coding_problems/geeksforgeeks/binary_search/EquilibriumPoint.java
+++ src/coding_problems/geeksforgeeks/binary_search/EquilibriumPoint.java
@@ -1,4 +1,51 @@
 package coding_problems.geeksforgeeks.binary_search;
 
+//https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/1
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class EquilibriumPoint {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static int equilibrium(int[] arr) {
+
+        int sum[] = new int[arr.length];
+        int leftSum = 0;
+        int rightSum = 0;
+        sum[0] = arr[0];
+        for (int k = 1; k < arr.length; k++)
+            sum[k] = arr[k] + sum[k - 1];
+
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            leftSum = sum[i] - arr[i];
+            rightSum = sum[arr.length - 1] - sum[i];
+
+
+            if (leftSum == rightSum)
+                return i;
+        }
+
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String[] input = br.readLine().split(" ");
+        int[] arr = new int[input.length];
+
+        for (int k = 0; k < arr.length; k++)
+            arr[k] = Integer.parseInt(input[k]);
+
+        int index = equilibrium(arr);
+
+        System.out.println(index);
+
+    }
 }
--- src/coding_problems/geeksforgeeks/binary_search/WaveArray.java
+++ src/coding_problems/geeksforgeeks/binary_search/WaveArray.java
@@ -1,4 +1,41 @@
 package coding_problems.geeksforgeeks.binary_search;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//https://www.geeksforgeeks.org/problems/wave-array-1587115621/1?track=amazon-arrays&batchId=192
+
 public class WaveArray {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+    public static void sortInWave(int arr[]) {
+        // code here
+        int temp = 0;
+        for (int i = 1; i < arr.length; i += 2) {
+            if (arr.length == 1) {
+                System.out.println(arr[0]);
+                break;
+            }
+            temp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+        }
+        for (int val : arr)
+            System.out.print(val + " ");
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        String[] input = br.readLine().split(" ");
+        int[] arr = new int[input.length];
+
+        for (int k = 0; k < arr.length; k++)
+            arr[k] = Integer.parseInt(input[k]);
+
+        sortInWave(arr);
+
+    }
 }
