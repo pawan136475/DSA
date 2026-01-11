52a603e0b500622cb1567657b6a70e4a6f1e5e33
pawan136475 <pawan36475@gmail.com>
2026-01-11 22:17:01 +0530

    code

---
+++ src/coding_problems/geeksforgeeks/binary_search/Array/sliding_window/SubstringswithKDistinct.java
@@ -0,0 +1,48 @@
+package coding_problems.geeksforgeeks.binary_search.Array.sliding_window;
+
+//https://www.geeksforgeeks.org/problems/count-number-of-substrings4528/1
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubstringswithKDistinct {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static int countSubstr(String s, int k) {
+        //  code here
+
+        int val1 = distinctIntegers(s, k);
+        int val2 = distinctIntegers(s, k - 1);
+        return val1 - val2;
+    }
+
+    public static int distinctIntegers(String s, int k) {
+        int l = 0, r = 0, count = 0;
+        Map<String, Integer> map = new HashMap<>();
+        while (r < s.length()) {
+            map.put(String.valueOf(s.charAt(r)), map.getOrDefault(String.valueOf(s.charAt(r)), 0) + 1);
+            while (map.size() > k) {
+                map.put(String.valueOf(s.charAt(l)), map.getOrDefault(String.valueOf(s.charAt(l)), 0) - 1);
+                if (map.get(String.valueOf(s.charAt(l))) == 0) map.remove(String.valueOf(s.charAt(l)));
+                l++;
+
+            }
+            count = count + r - l + 1;
+            r++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String input = br.readLine();
+        int k = Integer.parseInt(br.readLine());
+
+        int result = countSubstr(input, k);
+        System.out.println(result);
+    }
+}
