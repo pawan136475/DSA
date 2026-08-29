cf5930ad56b1c12a1273119a8efad8761f002d17
pawan136475 <pawan36475@gmail.com>
2026-08-29 21:10:42 +0530

    code

---
+++ src/LinkedListConcept/Main.java
@@ -0,0 +1,15 @@
+package LinkedListConcept;
+
+public class Main {
+    public static void main(String[] args) {
+        LL linkedList = new LL();
+        linkedList.insertFirst(1);
+        linkedList.insertFirst(2);
+        linkedList.insertFirst(3);
+        linkedList.insertFirst(4);
+        linkedList.insertLast(5);
+        linkedList.insert(7, 2);
+        linkedList.deleteFirst();
+        linkedList.display();
+    }
+}
